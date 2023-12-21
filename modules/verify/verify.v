088c5a4e471bd720a190ab2602617210a7480053
James Howe <jameshoweee@gmail.com>
2023-12-21 12:19:21 +0000

    Update verify.v

--- modules/verify/verify.v
+++ modules/verify/verify.v
@@ -1,10 +1,7 @@
-/*
- * This file is GetLeavesFromSiblingPath module which part of SDitH verify.
- *
- * Copyright (C) 2023
- * Authors: Sanjay Deshpande <sanjay.deshpande@sandboxquantum.com>
- *          
-*/
+// Copyright (c) SandboxAQ. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// This file is GetLeavesFromSiblingPath module which part of SDitH verify.
 
 `include "clog2.v"
 
